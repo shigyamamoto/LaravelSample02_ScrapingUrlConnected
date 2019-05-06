8109e775e405201eccdd99eb19fb6bd839d76184
shigeki.yamamoto <shigeki.yamamoto@b-attraction.com>
2019-05-06 19:19:45 +0900

    add factories

---
+++ database/factories/UrlFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\CollectedUrl;
+use Faker\Generator as Faker;
+
+$factory->define(CollectedUrl::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
